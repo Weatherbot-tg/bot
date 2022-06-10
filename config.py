6ac3f715eea8b066041a46891710ead96a4ce9fd
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2022-06-10 11:03:02 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-BOT_TOKEN = "<BOT-TOKEN>"
-OWM_TOKEN = "<OWM-TOKEN>"
+BOT_TOKEN = "<BOT-TOKEN>" #get here: t.me/botfather
+OWM_TOKEN = "<OWM-TOKEN>" #get here: https://openweathermap.org
+ADMIN_ID = "<admin chat id>" #to get type bot /id command in group or person chat 
